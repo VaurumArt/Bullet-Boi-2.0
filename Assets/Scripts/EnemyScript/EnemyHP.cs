2f2ba6c306932fe19292772a0b7cba9fb072562d
VaurumArt <justineimp22@gmail.com>
2026-05-19 22:42:58 +0800

    DownWardUpdate

--- Assets/Scripts/EnemyScript/EnemyHP.cs
+++ Assets/Scripts/EnemyScript/EnemyHP.cs
@@ -20,28 +20,19 @@ public class EnemyHP : MonoBehaviour
 
     ScoreSystemScript scoreSystemScript;
     EnemyParryBehaviour enemyParryBehaviour;
-    GameManager gameManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    private void Awake()
-    {
-       
-    }
     void Start()
     {
-        gameManager = FindAnyObjectByType<GameManager>();  
         scoreSystemScript = FindAnyObjectByType<ScoreSystemScript>();
         enemyParryBehaviour = GetComponent<EnemyParryBehaviour>();
         currentHealth = maxHealth;
 
         gameManager.EnemyAlive();
 
-
         if (bloodTransform == null)
             Debug.LogError("BloodTransform is NOT assigned!", this);
-    }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (currentHealth <= 0)
@@ -60,40 +51,35 @@ public class EnemyHP : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player"))
         {
-            if (collision.gameObject.TryGetComponent(out Rigidbody2D playerRb) &&
-                collision.gameObject.TryGetComponent(out PlayerMovement playerMovement))
+       Rigidbody2D playerRb =collision.gameObject.GetComponent<Rigidbody2D>();
+          
+            float playerSpeed = playerRb.linearVelocity.magnitude;
+            PlayerMovement playermovment = collision.gameObject.GetComponent<PlayerMovement>();
+            if (!playermovment.isKnockedBack && playerSpeed >= deathSpeed) //Check if the player speed is fast enough to kill the enemy 
             {
-                float playerSpeed = playerRb.linearVelocity.magnitude;
-
-                if (!playerMovement.isKnockedBack && playerSpeed >= deathSpeed)
-                {
-                    TakeDamage(20);
-                }
+                TakeDamage(20);
             }
-        }
 
+        }
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            if (collision.gameObject.TryGetComponent(out BulletScript bulletInfo))
-            {
-                TakeDamage(bulletInfo.bulletDamage);
-            }
+            BulletScript bulletInfo = collision.gameObject.GetComponent<BulletScript>();
+            TakeDamage(bulletInfo.bulletDamage);
         }
-
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Wall"))
         {
-            if (enemyParryBehaviour != null && enemyParryBehaviour.isParried)
+           if(enemyParryBehaviour.isParried)
             {
-                TakeDamage(2);
+                TakeDamage(5);
             }
         }
-    }
 
+
+    }
     public void Die()
     {
-        gameManager.EnemyDeath();
         deathFeedback?.PlayFeedbacks();
         scoreSystemScript.BiteKillScore();
         Debug.Log("Basic Enemy Died");
